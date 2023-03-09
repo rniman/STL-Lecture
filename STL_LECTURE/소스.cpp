90249fe729e358352662b1a0fcb24b82b097a547
rniman <shinmg00@gmail.com>
2023-03-09 17:58:04 +0900

    2-1 주차 수업

--- STL_LECTURE/소스.cpp
+++ STL_LECTURE/소스.cpp
@@ -1,38 +1,24 @@
 //------------------------------------------------------------
-// 2023. 3. 6 월56						(1주 2일)
+// 2023. 3. 9 목56						(2주 1일)
 //------------------------------------------------------------
 // 많은 수의 자료를 처기하기 int
 //------------------------------------------------------------
  
 #include <iostream>
-#include <random>
-#include <format>
+#include <array>
 #include "save.h"
-
-std::default_random_engine dre;
 
 //-------
 int main()
 //-------
 {
-	// 랜덤 int 1000개를 화면에 생성하여 화면에 출력하라
+	std::array<int, 1'000> a;
+	for (int i{}; i < 1'000; ++i)
+		a[i] = i + 1;
 
-	std::uniform_int_distribution<int> uid{ 0, 99999'9999 };
+	// a에 있는 int 1000개를 file에 저장하려고 한다
+	// [문제] file의 크기는 얼마나 될까?
 
-	// [문제] 1000개 중에서 가장 큰 수를 출력하라.
-
-	int max = -1;
-	
-	for (int i{}; i < 1000; ++i)
-	{
-		int rand_num = uid(dre);
-		std::cout << std::format("{:10}", rand_num);
-		if (max < rand_num)
-			max = rand_num;
-	}
-
-	std::cout << '\n' << "max_num = " << max << '\n';
 
 	save("소스.cpp");
 }
-
