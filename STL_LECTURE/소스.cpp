54b4ff4c6291644cb0d7d82d8df64b0dd31c53e4
rniman <shinmg00@gmail.com>
2023-03-16 19:00:26 +0900

    3-1주차 수업

--- STL_LECTURE/소스.cpp
+++ STL_LECTURE/소스.cpp
@@ -1,46 +1,51 @@
 //------------------------------------------------------------
-// 2023. 3. 13 월56						(2주 2일)
+// 2023. 3. 16 목56						(3주 1일)
 //------------------------------------------------------------
-// 많은 수의 자료를 처리하기 - class
+// 더 많은 자료 처리하기 - new를 사용한 메모리 동적할당 
 //------------------------------------------------------------
- 
 #include <iostream>
-#include <fstream>
-#include <array>
-#include <algorithm>
+#include <memory>
+#include <chrono>
 #include "save.h"
 
+// [문제] 스마트 포인터의 동작을 관찰할 수 있도록 class Dog를 사용
 
-// [문제] "int 천개"에는 int 값 1000개가 binary 모드로 4000바이트가 그대로 기록되어있다.
-// 메모리로 읽어와라.
-// 값이 100보다 작은 것은 모두 몇 개인가 출력하라.
+class Dog
+{
+public:
+	Dog();
+	~Dog();
+
+private:
+	char c[100'000'000];
+};
+
+Dog::Dog()
+{
+	std::cout << "Dog 생성" << '\n';
+}
+
+Dog::~Dog()
+{
+	std::cout << "Dog 소멸" << '\n';
+}
 
 //-------
 int main()
 //-------
 {
-	std::ifstream in{ "int 천개", std::ios::binary };
+	save("소스.cpp");
+	int cnt{};
+	while (true)
+	{
+		std::cout << ++cnt << '\n';
+		std::unique_ptr<Dog[]> dog{ new Dog[10] };    // sizeof(Dog) * 10 = 1GB
 
-	if (!in) {
-		exit(0);
+		//RAII
+		//제어경로 통일
+		//
+
+		
+		std::this_thread::sleep_for(1s);
 	}
-
-	std::array<int, 1'000> numbers;
-	in.read((char*)&numbers, sizeof numbers);
-
-	// 다음 시간에는 알고리즘으로 해결
-
-	int cnt = std::count_if(numbers.begin(), numbers.end(), [](int n) {return n < 100; });
-
-	//int cnt{};
-
-	//for (int num : numbers) {
-	//	if (num < 100) {
-	//		++cnt;
-	//	}
-	//}
-
-	std::cout << "100보다 작은 숫자 - " << cnt << '\n';
-
-	//save("소스.cpp");
 }
