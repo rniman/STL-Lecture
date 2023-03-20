190cc9d9fd0d05bdf23395b06df5b5293ca1d157
rniman <shinmg00@gmail.com>
2023-03-20 16:09:13 +0900

    3주차 수업 끝

--- STL_LECTURE/소스.cpp
+++ STL_LECTURE/소스.cpp
@@ -1,46 +1,60 @@
 //------------------------------------------------------------
-// 2023. 3. 16 목56						(3주 1일)
+// 2023. 3. 20 월56						(3주 2일)
+// 4. 3 예비군 훈련 - 수업 강의녹화 영상을 e-class에 제공
 //------------------------------------------------------------
-// 더 많은 자료 처리하기 - new를 사용한 메모리 동적할당 
+// 정렬(sort) - callable type
 //------------------------------------------------------------
 #include <iostream>
-#include <memory>
-#include <chrono>
+#include <array>
+#include <random>
+#include <format>
+#include <algorithm>
+#include <ranges>		// C++ 20
 #include "save.h"
 
-// [문제] 스마트 포인터의 동작을 관찰할 수 있도록 class Dog를 사용
+// [문제] int 100만개를 만들고 random 값으로 채워라
+// 오름차순으로 정렬하라
+// 값을 출력하라
 
-class Dog
-{
+
+std::array<int, 1'000'000> numbers;
+std::default_random_engine dre;
+std::uniform_int_distribution uid{ 1, 1'000'000 };
+
+//bool 어떻게( int a, int b)
+//{
+//	return a > b;
+//}
+class Dog {
 public:
-	Dog();
-	~Dog();
-
-private:
-	char c[100'000'000];
+	bool operator()(int a, int b)
+	{
+		return a > b;
+	}
 };
-
-Dog::Dog()
-{
-	std::cout << "Dog 생성" << '\n';
-}
-
-Dog::~Dog()
-{
-	std::cout << "Dog 소멸" << '\n';
-}
 
 //-------
 int main()
 //-------
-{
-	save("소스.cpp");
-	int cnt{};
-	while (true)
+{	
+	for (int& number : numbers)
 	{
-		std::cout << ++cnt << '\n';
-		std::unique_ptr<Dog[]> dog{ new Dog[10] };    // sizeof(Dog) * 10 = 1GB
-
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		number = uid(dre);
 	}
+
+	//오름차순 정렬
+	// 3번째 인자가 디폴트 인자 오름차순 정렬 -> 정렬 방법은 sort에게 직접 알려줘야 한다.(함수를 인자로 전다)
+	// 3번째 인자의 타입은 무한개이다. (부를 수 있는 타입, callable type)
+	std::sort(numbers.begin(), numbers.end(), Dog());	// O(nlog(n))
+	//std::ranges::sort(numbers); //C++ 20이상
+
+	// 뒤에서 100개만 출력
+	for (int number : numbers | 				  // -> for(auto i = numbers.begin(); numbers.end(); ++i)로 번역 ( syntatic sugar )
+			std::views::drop(numbers.size()-100)) // | filter를 통해서 
+	{
+		std::cout << std::format("{:8}", number);
+	}
+
+	save("소스.cpp");
 }
+
