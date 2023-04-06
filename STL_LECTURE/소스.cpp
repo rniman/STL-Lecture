c2036f55ac3b32d744dd2bb87bb82595bcb51bcd
rniman <105342620+rniman@users.noreply.github.com>
2023-04-06 22:12:27 +0900

    6-1주차 메모정리

--- STL_LECTURE/소스.cpp
+++ STL_LECTURE/소스.cpp
@@ -4,7 +4,8 @@
 // Sequence container - vector
 //------------------------------------------------------------
 #include <iostream>
-#include <vector>#include "save.h"
+#include <vector>
+#include "save.h"
 #include "String.h"
 
 using namespace std;
@@ -14,11 +15,14 @@ int main()
 //-------
 {	
 	관찰 = true;
-	vector<String> v{ "123", "abced", "7890" };
+
+	vector<String> v;
+	v.reserve(10);
+	v.emplace_back("123");
+	v.emplace_back("abcde");
+	v.emplace_back("7890");
 
 	//메모리 그림으로 그릴 수 있니?
-
-
 
 	//save("소스.cpp");
 }
