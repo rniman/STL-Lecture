d31e472c6b15354d8949e2dbb447f4b627e4fcc1
rniman <shinmg00@gmail.com>
2023-05-04 13:19:51 +0900

    9주차 메모 정리

--- STL_LECTURE/소스.cpp
+++ STL_LECTURE/소스.cpp
@@ -1,24 +1,41 @@
 //------------------------------------------------------------
-// 2023. 4. 27 목56								(9주 1일)
+// 2023. 5. 4 목56								(10주 1일)
 //------------------------------------------------------------
-// forward_list 
-// It is intended that forward_list have zero space or time overhead
-// relative to a hand - written C - style singly linked list.
-// Features that would conflict with that goal have been omitted.
+// 반복자(iterator)
+// - 포인터를 추상화(=일반화) 한 것이 반복자다
+// - Iterators are a generalization of pointers
+// 6가지 종류(category)로 나뉜다.
+// - 입력/출력/전진(전방향)/양방향(전후진)/랜덤억세스/컨티구어스 반복자
+// 기초동작: *, ++, =
 //------------------------------------------------------------
 #include <iostream>
+#include <vector>
+#include <iterator>
 #include "save.h"
 #include "String.h"
 
 using namespace std;
 extern bool 관찰;
 
+// [문제] 함수 f() 반복자를 인자로 받아 어떤 종류의 반복자인지 화면 출력한다.
+// 
+// 전달된 인자가 원하는 타입인지 판단하는 - type_traits 
+template <class iter>
+void f(iter)
+{
+	cout << typeid(iter).name() << endl;
+}
+
 //-------
 int main()
 //-------
 {
-	//forward_list 는 size조차 지원하지 않는다.
+	vector<int> v;
+	ostream_iterator<char> p{ cout };
+	
+	f(123);
+	f(v.begin());
+	f(p);
 
 	save("소스.cpp");
 }
-
